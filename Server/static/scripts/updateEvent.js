b6420c1b1f975448448af4f57575bd52da0aef42
Tom Sharp <tomsharp924@gmail.com>
2025-11-10 19:02:37 -0800

    Reimplemented all current logic with microservices and sqlite

--- Server/static/scripts/updateEvent.js
+++ Server/static/scripts/updateEvent.js
@@ -5,18 +5,9 @@ const content = document.getElementById("content");
 const loading = document.getElementById("loading");
 
 function formatTimeForInput(timeStr) {
-    // Example: "2:30 PM" -> "14:30"
-    const [time, modifier] = timeStr.split(" "); // ["2:30", "PM"]
-    let [hours, minutes] = time.split(":").map(Number);
+    if (!timeStr) return "";
+    return timeStr.slice(0,5); // take only HH:MM
 
-    if (modifier === "PM" && hours < 12) hours += 12;
-    if (modifier === "AM" && hours === 12) hours = 0;
-
-    // pad with zero
-    hours = hours.toString().padStart(2, "0");
-    minutes = minutes.toString().padStart(2, "0");
-
-    return `${hours}:${minutes}`;
 }
 
 
@@ -25,13 +16,14 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.json())
         .then(data => { 
             //console.log(data);
-            document.getElementById("eventName").value = data.event.eventName;
-            document.getElementById("eventDate").value = data.event.eventDate;
-            document.getElementById("startTime").value = formatTimeForInput(data.event.startTime);
-            document.getElementById("endTime").value = formatTimeForInput(data.event.endTime);
-            document.getElementById("location").value = data.event.eventLocation;
-            document.getElementById("description").value = data.event.eventDescription;
-
+            document.getElementById("eventName").value = data.event.event_name;
+            document.getElementById("eventDate").value = data.event.event_date;
+            document.getElementById("startTime").value = formatTimeForInput(data.event.start_time);
+            document.getElementById("endTime").value = formatTimeForInput(data.event.end_time);
+            document.getElementById("location").value = data.event.event_location;
+            document.getElementById("description").value = data.event.event_description;
+            document.getElementById("timezone").value = data.event.tz_str;
+            
             if(data.event.hasOwnProperty("age_restriction")) {
                 document.getElementById("age_restriction").value = Number(data.event.age_restriction);
             }
