1d1693626c704af1be15669766549e0621c8c77f
Tom Sharp <tomsharp924@gmail.com>
2025-11-20 17:07:05 -0800

    Added delete functionality

---
+++ test-media.py
@@ -0,0 +1,26 @@
+import requests 
+
+url = "http://localhost:5004/upload"
+url2 = "http://localhost:5004/delete"
+image_path = "test-image.jpg"
+url_id = ""
+
+with open(image_path, "rb") as f:
+    files = {
+        "file": (image_path, f, "image/jpeg")
+    }
+    response = requests.post(url, files=files)
+
+    try:
+        data = response.json()
+        url_id = data["url_id"]
+        print(data)
+    except:
+        print (response.text)
+
+response2 = requests.post(f"{url2}/{url_id}")
+try:
+    data = response2.json()
+    print(data)
+except:
+    print (response2.text)
